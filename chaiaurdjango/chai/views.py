a6de8bae2bd2e7309fb06a6e3ebccf27dc9581d9
Waseem194 <bsf1801678@ue.edu.pk>
2024-06-08 13:53:35 +0500

    djanoadmin

--- chaiaurdjango/chai/views.py
+++ chaiaurdjango/chai/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import ChaiVariety
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 
@@ -7,3 +8,10 @@ def all_chai(request):
     chais = ChaiVariety.objects.all()
     return render(request, 'chai/all_chai.html',{'chais': chais})
 
+# def chai_detail(request, chai_id):
+#     chai = get_object_or_404(ChaiVariety, pk= chai_id)
+#     return render(request, 'chai/chai_detail.html', {'chai': chai})
+
+def chai_detail(request, chai_id):
+  chai = get_object_or_404(ChaiVariety, pk=chai_id)
+  return render(request, 'chai/chai_detail.html', {'chai': chai})
